ad3954255d09dbf82387f3db49377e563738cf15
Kevin Hill <kevinkhill@gmail.com>
2019-12-17 13:57:32 -0800

    missed some merges

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,6 +6,13 @@ export interface Point {
   [K: string]: number | undefined;
 }
 
+export interface Position {
+  X: number;
+  Y: number;
+  Z: number;
+  B: number;
+}
+
 export interface CannedPoint extends Point {
   I?: number;
   J?: number;
