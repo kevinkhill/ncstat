337e98c1f0576fc94b0941e8a381a017cad12d0e
Kevin Hill <kevinkhill@gmail.com>
2020-02-10 20:40:41 -0800

    adding readme

--- src/NcBlock/index.ts
+++ src/NcBlock/index.ts
@@ -1,3 +1,3 @@
 export { getBlockGenerator } from "../NcBlock/blockGenerator";
-export { parseLine, getBlocks } from "../NcBlock/getBlocks";
+export { getBlocks } from "../NcBlock/getBlocks";
 export { NcBlock } from "../NcBlock/NcBlock";
