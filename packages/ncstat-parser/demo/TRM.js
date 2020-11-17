f99712c92433ed4cb6dc78a62e26545c23c775dd
Kevin Hill <kevinkhill@gmail.com>
2020-11-17 04:36:08 -0800

    removing types from demo

--- packages/ncstat-parser/demo/TRM.js
+++ packages/ncstat-parser/demo/TRM.js
@@ -6,7 +6,7 @@ import { NcParser, NcParserConfig, defineGCode } from "../build";
 const DEMO_FILE = "VF10.NC";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const writeFile = (file: string, data: any): Promise<void> => {
+const writeFile = (file, data) => {
   const outFile = path.join(__dirname, "output", file);
 
   console.log(`Writing to ${outFile}`);
@@ -15,20 +15,18 @@ const writeFile = (file: string, data: any): Promise<void> => {
 };
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const readFile = (file: string): Promise<string> =>
-  fs.promises.readFile(path.join(__dirname, file), "utf8");
+const readFile = file => fs.promises.readFile(path.join(__dirname, file), "utf8");
 
 (async () => {
-  const config: NcParserConfig = {
+  const parser = new NcParser({
     debug: true,
     lexerConfig: {
       tokens: {
         EOF: false
       }
     }
-  };
+  });
 
-  const parser = new NcParser(config);
   const contents = await readFile(DEMO_FILE);
   const program = parser.parse(contents);
   const json = JSON.stringify(program);
