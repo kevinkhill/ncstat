5270c75981623ce1a582837ce2612af9a452e296
Kevin Hill <kevinkhill@gmail.com>
2020-11-27 14:43:17 -0800

    testing ignoring nc files on github

--- cli/src/BaseCommand.ts
+++ cli/src/BaseCommand.ts
@@ -3,7 +3,8 @@ import { Command } from "clipanion";
 import { MyContext } from "./context";
 
 export default abstract class BaseCommand extends Command<MyContext> {
-  cwd = Command.String(`--cwd`, { hidden: true });
+  @Command.String(`--cwd`, { hidden: true })
+  cwd!: string;
 
   abstract execute(): Promise<number | void>;
 }
